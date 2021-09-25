0820d9786401c0ec98ccb1159ec45a25c7b48c76
kamadiem <kamadiem@uncc.edu>
2021-09-25 03:28:59 -0400

    code update

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const bp = require('body-parser')
+
 const app = express();
 const port = 3000
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 const mariadb = require('mariadb');
 let conn;
 
 async function createConnection() {
-  if(!conn) {
+  if (!conn) {
     try {
       conn = await mariadb.createPool({
         host: 'localhost',
@@ -15,11 +20,11 @@ async function createConnection() {
         database: 'javatpoint',
         port: 3306,
         connectionLimit: 5
-    }).getConnection();
-    } catch(err) {
+      }).getConnection();
+    } catch (err) {
       console.log('connection not created', err)
     } finally {
-      if(conn) {
+      if (conn) {
         conn.end();
       }
     }
@@ -27,7 +32,7 @@ async function createConnection() {
   }
 }
 
-app.get('/student',async (req, res) => {
+app.get('/student', async (req, res) => {
   await createConnection();
   const val = await conn.query("select * from student;");
   console.log("val is: ", val);
@@ -35,16 +40,34 @@ app.get('/student',async (req, res) => {
   res.end(JSON.stringify(val))
 })
 
-app.post('/student', async, (req, res) => {
-  const {body} = req;
-  const {name= '', titie= '', className = '', section = '', rollId = ''} = body
-    const val = await conn.query(`update student SET name = ${name}, title = ${title}, class = ${className}, section =${section} where rollId = ${rollId}`);
-    res.setHeader('Content-Type', 'application/json')
-    console.log("val is: ", val);
-    res.end(JSON.stringify(val)) 
+app.post('/student', (req, res) => {
+  const { body } = req;
+  const { name = '', titie = '', className = '', section = '', rollId = '' } = body
+  const val = await conn.query(`update student SET name = ${name}, title = ${title}, class = ${className}, section =${section} where rollId = ${rollId}`);
+  res.setHeader('Content-Type', 'application/json')
+  console.log("val is: ", val);
+  res.end(JSON.stringify(val))
 });
 
-app.get('/customer',async (req, res) => {
+app.patch('/student', (req, res) => {
+  const { body } = req;
+  const { name = '', titie = '', className = '', section = '', rollId = '' } = body
+  const val = await conn.query(`update student SET name = ${name}, title = ${title}, class = ${className}, section =${section} where rollId = ${rollId}`);
+  res.setHeader('Content-Type', 'application/json')
+  console.log("val is: ", val);
+  res.end(JSON.stringify(val))
+});
+
+app.delete('/student', (req, res) => {
+  const { body } = req;
+  const { rollId = '' } = body
+  const val = await conn.query(`Delete from student where rollId = ${rollId}`);
+  res.setHeader('Content-Type', 'application/json')
+  console.log("val is: ", val);
+  res.end(JSON.stringify(val))
+});
+
+app.get('/customer', async (req, res) => {
   await createConnection();
   const val = await conn.query("select * from customer;");
   console.log("val is: ", val);
@@ -52,7 +75,7 @@ app.get('/customer',async (req, res) => {
   res.end(JSON.stringify(val))
 })
 
-app.get('/company',async (req, res) => {
+app.get('/company', async (req, res) => {
   await createConnection();
   const val = await conn.query("select * from company;");
   console.log("val is: ", val);
@@ -62,5 +85,5 @@ app.get('/company',async (req, res) => {
 
 
 app.listen(port, () => {
-      console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 });
